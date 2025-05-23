a48ce6c992b6c04b1ac4039d42c88ab1994be462
Jiwon Lim <63719590+Sup-xort@users.noreply.github.com>
2025-05-23 21:28:46 +0900

    Update Sensor.py

--- Sensor.py
+++ Sensor.py
@@ -1,7 +1,6 @@
 import serial
 
-# 시리얼 포트 설정 (환경에 따라 수정)
-ser = serial.Serial("COM6", 115200)
+ser = serial.Serial("/dev/ttyAMA0", 115200)
 ser.reset_input_buffer()
 
 def get_rri():
@@ -14,6 +13,6 @@ def get_rri():
             if line.isdigit():
                 rri = int(line)
                 if 200 < rri < 2000:
-                    return rri  # ✅ 유효한 값이면 바로 반환
+                    return rri
         except:
-            continue  # 에러 발생 시 무시하고 계속 대기
+            continue
